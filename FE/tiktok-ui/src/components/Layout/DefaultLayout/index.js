8e68d4a2a211fc02b6b1941fd45f636ba1ce1723
hudagon <dohoangduc1112000@gmail.com>
2023-04-04 22:39:53 +0700

    nopbai

--- FE/tiktok-ui/src/components/Layout/DefaultLayout/index.js
+++ FE/tiktok-ui/src/components/Layout/DefaultLayout/index.js
@@ -1,15 +1,19 @@
 import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import styles from "./DefaultLayout.module.scss"
+import classNames from "classnames/bind";
+import Sidebar from "./Sidebar";
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({
     children
 }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
